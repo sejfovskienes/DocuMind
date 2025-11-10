fab4a4ab43ed170bb800b82a194be9582b1dad4e
sejfovskienes <enes22seyfovski@gmail.com>
2025-11-10 20:36:26 +0100

    [feat] get-document-metadata endpoint added and tested

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routes import auth, files
+from app.routes import auth, files, process_document
 from app.database import Base, engine
 # from app.models import user as user_model
 
@@ -14,3 +14,4 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(files.router)
+app.include_router(process_document.router)

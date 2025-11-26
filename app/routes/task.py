7b9a0f2ca1f1d908de3b6bdad4880249a91e2ee7
sejfovskienes <enes22seyfovski@gmail.com>
2025-11-26 20:52:16 +0100

    ner task endpoint added

--- app/routes/task.py
+++ app/routes/task.py
@@ -6,13 +6,23 @@ from app.database import get_database_session
 from app.routes.auth import get_current_user
 from app.services import task_service
 from app.models.user import User
+from app.models.worker_task import WorkerTask
 
 router = APIRouter(tags=["Worker Task"])
 
-@router.get("get-task/{task-id}")
+@router.get("/get-task/{task-id}")
 def get_worker_task(
     task_id: int, 
     db: Session = Depends(get_database_session), 
     user: User = Depends(get_current_user)):
     worker_task = task_service.get_worker_task_by_id(db, task_id)
     return {"message": worker_task}
+
+@router.get("/ner-task/{document_id}")
+def make_ner_task(
+    document_id: int, 
+    db: Session = Depends(get_database_session), 
+    user: User = Depends(get_current_user)):
+    ner_task = WorkerTask(payload={"document_id": document_id}, task_type="ner-task")
+    task_service.save_worker_task(db, ner_task)
+    return{"message": "Task saved successfully!", "task": ner_task}

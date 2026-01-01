6eb6ff692cc10ce249a461d6475a48d3ffe51d46
sejfovskienes <enes22seyfovski@gmail.com>
2026-01-01 15:49:46 +0100

    summarization endpoint added

--- app/routes/nlp.py
+++ app/routes/nlp.py
@@ -29,3 +29,15 @@ def get_task_by_id(
     worker_task = task_service.get_task_by_id(db, task_id)
     return {
         "worker_task": WorkerTaskSchema.model_validate(worker_task)}
+
+@router.get("/summarization/{document_id}")
+def summarization_endpoint(
+    document_id: int, 
+    db: Session = Depends(get_database_session), 
+    current_user: User = Depends(get_current_user)) -> dict[str, Any]:
+    summarization_task = WorkerTask(
+        payload={"document_id": document_id},
+        task_type=WorkerTaskType.SUMMARIZATION,
+    )
+    task_service.save_worker_task(db, summarization_task)
+    return {"summarization_task": WorkerTaskSchema.model_validate(summarization_task)}
